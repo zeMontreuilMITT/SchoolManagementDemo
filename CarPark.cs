31208daa76492f059fc3a790618e5a74f2a50480
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2023-01-25 14:01:08 -0600

    Initial Carpark commit

---
+++ CarPark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementDemo
+{
+    public static class CarPark
+    {
+        private static HashSet<Vehicle> Vehicles = new HashSet<Vehicle>();
+        public static int Capacity { get; }
+
+        // static class constructor is invoked when the program runs
+        static CarPark()
+        {
+            Capacity = 300;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-﻿using SchoolManagementDemo;
-
-try
-{
-    Course software = new Course(10, "Software Developer", 6);
-
-    Student phil = new Student("Phil Wiggins", new DateTime(1991, 10, 10), "Spain");
-    Student bob = new Student("Bob Wiggins", new DateTime(1991, 10, 10), "Spain");
-    Student harry = new Student("Harry Wiggins", new DateTime(1991, 10, 10), "Spain");
-
-    // "BIDIRECTIONAL relationship" -- both objects can refer to each other
-
-    software.RegisterStudent(phil);
-    software.RegisterStudent(bob);
-    software.RegisterStudent(harry);
-
-    Console.WriteLine(software.GetStudentList().Keys.Count);
-    Console.WriteLine($"{bob.Name} is registered in the course {bob.Course.Name} with {bob.Course.GetStudentList().Keys.Count} other students.");
-
-} catch(Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
+﻿/// Create an OOP "Carpark" System
+/// At its most basic, the static CarPark class will have a private HashSet of Vehicles, and a method: public static void Park (Vehicle vehicle) method.
+/// Vehicles have a string LicenseNumber, and a string ParkingSpot
+/// Both should be private and validated with public properties
+/// 
+/// When the Park method is invoked, it adds the vehicle to the CarPark HashSet,  counts the vehicles in the HashSet, and uses that number to assign a spot.
+/// If 20 vehicles are parked, then the 21st vehicle parked is given spot 21.
+/// 
+/// It should prevent a vehicle from parking in more than one spot (if it already has a spot) or if the spots are over capacity.
