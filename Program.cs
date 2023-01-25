f71a893c0ffa729e808a725b15b217ab46415da9
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2023-01-25 13:53:48 -0600

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,122 +1,23 @@
-﻿class Course
+﻿using SchoolManagementDemo;
+
+try
 {
-    private int _duration;
-    public int Duration
-    {
-        get { return _duration; }
-        set
-        {
-            if (value > 0)
-            {
-                _duration = value;
-            }
-            else
-            {
-                throw new Exception("Duration must exceed zero");
-            }
-        }
-    }
+    Course software = new Course(10, "Software Developer", 6);
 
+    Student phil = new Student("Phil Wiggins", new DateTime(1991, 10, 10), "Spain");
+    Student bob = new Student("Bob Wiggins", new DateTime(1991, 10, 10), "Spain");
+    Student harry = new Student("Harry Wiggins", new DateTime(1991, 10, 10), "Spain");
 
-    private string _name;
-    public string Name
-    {
-        get { return _name; }
-        set
-        {
-            if (value.Length < 2)
-            {
-                throw new Exception("Name must have more than one character.");
-            }
-            else
-            {
-                _name = value;
-            }
-        }
-    }
+    // "BIDIRECTIONAL relationship" -- both objects can refer to each other
 
+    software.RegisterStudent(phil);
+    software.RegisterStudent(bob);
+    software.RegisterStudent(harry);
 
-    private int _credits;
-    public int Credits
-    {
-        get { return _credits; }
-        set
-        {
-            if (value > 0)
-            {
-                _credits = value;
-            }
-            else if (value > MaxCredits)
-            {
-                throw new Exception($"Credits cannot exceed maximum of {MaxCredits}");
-            }
-            else
-            {
-                throw new Exception("Duration must exceed zero");
+    Console.WriteLine(software.GetStudentList().Keys.Count);
+    Console.WriteLine($"{bob.Name} is registered in the course {bob.Course.Name} with {bob.Course.GetStudentList().Keys.Count} other students.");
 
-            }
-        }
-    }
-
-    private static int MaxCredits = 6;
-    public Course(int duration, string name, int credits)
-    {
-        Duration = duration;
-        Name = name;
-        Credits = credits;
-    }
-}
-
-class Student
+} catch(Exception ex)
 {
-    private string _name;
-    public string Name
-    {
-        get { return _name; }
-        set
-        {
-            if (value.Length < 2)
-            {
-                throw new Exception("Name must have more than one character.");
-            }
-            else
-            {
-                _name = value;
-            }
-        }
-    }
-
-    // only initialized in Constructor, readonly
-    private DateTime _birthdate;
-    public DateTime Birthdate { get { return _birthdate; } }
-
-    private string _nationality;
-    public string Nationality { get { return _nationality; } }
-
-    public Student(string name, DateTime birthDate, string nationality)
-    {
-        Name = name;
-
-        // validate age is less than 100
-        DateTime currentDate = DateTime.Now;
-        TimeSpan span = currentDate - birthDate;
-        if (span.Days / 365 > 100)
-        {
-            throw new Exception("Birthdate cannot exceed 100 years prior.");
-        }
-        else
-        {
-            _birthdate = birthDate;
-        }
-
-        // validate nationality
-        if (nationality.Length < 2)
-        {
-            throw new Exception("Nationality input should be greater than one character");
-        }
-        else
-        {
-            _nationality = nationality;
-        }
-    }
+    Console.WriteLine(ex.Message);
 }
---
+++ School.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementDemo
+{
+    public static class School
+    {
+        // static name, hashset of courses and of students
+        // register new student(all student constructor properties), register new course methods (all course constructor properties) and add to hashsets
+    }
+}
